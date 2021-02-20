2fca034ebd8c95f84c23c8cc1b2922bffda4f590
vigneshvp <viki3110@gmail.com>
2021-02-20 17:03:48 +0530

    Created modal for Signin
    Added script and styling for modal

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+function showSignInModal(){
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "flex";
+  }
+  
+  function hideSignInModal(){
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "none";
+  }
