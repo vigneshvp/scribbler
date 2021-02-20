c5735f498b20e57bf02a6cb1bed752206142a347
vigneshvp <viki3110@gmail.com>
2021-02-20 17:14:31 +0530

    Created Signup Modal
    Added styling and JS for signup modal.

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,14 @@ function showSignInModal(){
     var modal = document.getElementById("signin-modal");
     modal.style.display = "none";
   }
+
+  function showSignUpModal(){
+    var modal = document.getElementById("signup-modal");
+    modal.style.display = "flex";
+  }
+  
+  function hideSignUpModal(){
+    var modal = document.getElementById("signup-modal");
+    modal.style.display = "none";
+  }
+  
