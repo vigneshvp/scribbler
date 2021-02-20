ed2327f7273bef4457953b6b2f37debd7f8de3c5
vigneshvp <viki3110@gmail.com>
2021-02-20 17:36:33 +0530

    Created modal for Add Post
    Added Styles and Scripts for Add post

--- scripts/index.js
+++ scripts/index.js
@@ -17,4 +17,19 @@ function showSignInModal(){
     var modal = document.getElementById("signup-modal");
     modal.style.display = "none";
   }
+
+  function showCreatePost(){
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "flex";
+  }
+  
+  function hideCreatePostModal(){
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "none";
+  }
+  
+  function navigateToBlogList(){
+    location.href = "html/bloglist.html"
+  }
+
   
