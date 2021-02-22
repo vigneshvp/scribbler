f6688c87d798698c73aecb2f34317ffdc7486696
vigneshvp <viki3110@gmail.com>
2021-02-22 11:37:47 +0530

    Refactored header code to common.css and common.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,3 @@
-function showSignInModal(){
-    var modal = document.getElementById("signin-modal");
-    modal.style.display = "flex";
-  }
-  
-  function hideSignInModal(){
-    var modal = document.getElementById("signin-modal");
-    modal.style.display = "none";
-  }
-
-  function showSignUpModal(){
-    var modal = document.getElementById("signup-modal");
-    modal.style.display = "flex";
-  }
-  
-  function hideSignUpModal(){
-    var modal = document.getElementById("signup-modal");
-    modal.style.display = "none";
-  }
-
   function showCreatePostModal(){
     var modal = document.getElementById("create-post-modal");
     modal.style.display = "flex";
