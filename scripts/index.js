94a63292ba42414bb92b16ed255617ff34906b35
vigneshvp <viki3110@gmail.com>
2021-02-22 17:42:52 +0530

    Code quality and Formatting

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
-  function showCreatePostModal(){
-    var modal = document.getElementById("create-post-modal");
-    modal.style.display = "flex";
-  }
-  
-  function hideCreatePostModal(){
-    var modal = document.getElementById("create-post-modal");
-    modal.style.display = "none";
-  }
-  
-  function viewAllPosts(){
-    location.href = "postslist.html"
-  }
+function showCreatePostModal() {
+  var modal = document.getElementById("create-post-modal");
+  modal.style.display = "flex";
+}
+
+function hideCreatePostModal() {
+  var modal = document.getElementById("create-post-modal");
+  modal.style.display = "none";
+}
+
+function viewAllPosts() {
+  location.href = "postslist.html"
+}
 
   
--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,36 +1,26 @@
+/* View the complete post , redirects to post.html page */
 function viewPostDetails(index) {
-    var postUserName = document.getElementsByClassName('post-username')[index].innerHTML;
-    var postTitle = document.getElementsByClassName('post-title')[index].innerHTML;
-    var postDesc = document.getElementsByClassName('post-desc')[index].innerHTML;
-    sessionStorage.setItem("postUserName",postUserName);
-    sessionStorage.setItem("postTitle",postTitle);
-    sessionStorage.setItem("postDesc",postDesc);
-    location.href = "post.html";
+  var postUserName = document.getElementsByClassName(
+      'post-username')[index].innerHTML;
+  var postTitle = document.getElementsByClassName(
+      'post-title')[index].innerHTML;
+  var postDesc = document.getElementsByClassName('post-desc')[index].innerHTML;
+  sessionStorage.setItem("postUserName", postUserName);
+  sessionStorage.setItem("postTitle", postTitle);
+  sessionStorage.setItem("postDesc", postDesc);
+  location.href = "post.html";
 }
 
-/* Code for Trash Modal */
-var trashFunctions = function(numberOfPosts) {
-
-    for (var i = 0; i < numberOfPosts; i++) {
-        // Get the trash modal
-        var modal = document.getElementsByClassName('trash-modal')[i];
-
-        // Get the div that opens the modal
-        var div = document.getElementsByClassName("post-trash")[i];
-
-        // Get the <span> element that closes the modal
-        var close = document.getElementsByClassName("trash-modal-button-no")[i];
-
-        // When the user clicks on the trash icon, open the modal 
-        div.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on `No` button, close the modal
-        close.onclick = function() {
-            modal.style.display = "none";
-        }
-    }
+/* Show Delete Modal */
+function showDeleteModal(index) {
+  var modal = document.getElementsByClassName("trash-modal")[index];
+  modal.style.display = "flex";
 }
 
-trashFunctions(5)
+/* Hide Delete Modal */
+function hideDeleteModal(index) {
+  var modal = document.getElementsByClassName("trash-modal")[index];
+  modal.style.display = "none";
+}
+
+
