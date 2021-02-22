e2a5ed4e34004a348e1a20ffc39f6e5f2772ad9b
vigneshvp <viki3110@gmail.com>
2021-02-22 13:36:19 +0530

    Added logic to navigate to Post by pressing ellipsis button

--- scripts/post.js
+++ scripts/post.js
@@ -1,25 +1,14 @@
-function showSignInModal(){
-    var modal = document.getElementById("signin-modal");
-    modal.style.display = "flex";
-  }
-  
-  function hideSignInModal(){
-    var modal = document.getElementById("signin-modal");
-    modal.style.display = "none";
-  }
-  
-  function showSignUpModal(){
-    var modal = document.getElementById("signup-modal");
-    modal.style.display = "flex";
-  }
-  
-  function hideSignUpModal(){
-    var modal = document.getElementById("signup-modal");
-    modal.style.display = "none";
-  }
+/* Fetches the details of the post and loads it */
+function onLoad() {
+  document.getElementsByClassName('postAuthor')[0].innerHTML = sessionStorage.getItem('postUserName');
+  document.getElementById('blogTitleNew').innerHTML = sessionStorage.getItem('postTitle');
+  document.getElementById('blogBody').innerHTML = sessionStorage.getItem('postDesc');
+}
 function editComment(){
   
 }
+
+
 function incrementCounter(){
   var cntr = document.getElementById('counterLikedBy');
   var val = parseInt(cntr.innerText);
